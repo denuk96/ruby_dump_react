a55838724155820dcc4c951e547ae9ccd6273fe4
Andrii <arozputnii@gmail.com>
2021-01-16 15:41:34 +0200

    styles for posts

--- src/components/post/postItem.js
+++ src/components/post/postItem.js
@@ -1,19 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link, useRouteMatch } from "react-router-dom";
+import styles from "./Post.module.scss";
 
 function PostItem({ post, category }) {
   let match = useRouteMatch();
 
   return (
-    <div>
-      <b>title: {post.title}</b>
-      <p>category_id: {post.category_id}</p>
-      {category ? (
-        <Link to={`${match.url}/${category.name}/${post.title}`}>show</Link>
-      ) : (
-        <Link to={`${match.url}/other/${post.title}`}>show</Link>
-      )}
+    <div className={styles.post}>
+        <div className={styles.post__card}>
+            <h5 className={"mt-2 text-center text-white"}>{post.title}</h5>
+            <p className={"p-3 text-white-100"}>{post.body}</p>
+            {category ? (
+                <Link to={`${match.url}/${category.name}/${post.title}`}>show</Link>
+            ) : (
+                <Link to={`${match.url}/other/${post.title}`}>show</Link>
+            )}
+        </div>
+
     </div>
   );
 }
