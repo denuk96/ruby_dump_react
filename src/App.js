6df7f08304b7e6aba79053b7e4b240f52a51d512
Denys <denys.taradada.triare@gmail.com>
2021-01-16 17:45:46 +0200

    some styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Router } from "react-router-dom";
 import history from "./config/history";
 import Header from "./components/common/header/header";
 import Routes from "./config/routes";
-import Message from "./components/common/message";
+import Message from "./components/common/message/message";
 import "./styles/index.scss";
 
 export default function App() {
