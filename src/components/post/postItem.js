30e88b6549c2b6766c24d67bd5ef1d7c6404facd
arozputnii <a.rpzutnii@gmail.com>
2021-01-20 22:26:00 +0200

    add link

--- src/components/post/postItem.js
+++ src/components/post/postItem.js
@@ -3,23 +3,38 @@ import PropTypes from "prop-types";
 import { Link, useRouteMatch } from "react-router-dom";
 import styles from "./Post.module.scss";
 
+function trimText(text, length = 100, end = ' ...') {
+    let index = text.indexOf(" ", length);
+    if( index === -1 ) index = length;
+
+    return text.slice(0, index) + end;
+}
+
 function PostItem({ post, category }) {
   let match = useRouteMatch();
 
-  return (
-    <div className={styles.post}>
-        <div className={styles.post__card}>
-            <h5 className={"mt-2 text-center text-white"}>{post.title}</h5>
-            <p className={"p-3 text-white-100"}>{post.body}</p>
-            {category ? (
-                <Link to={`${match.url}/${category.name}/${post.title}`}>show</Link>
-            ) : (
-                <Link to={`${match.url}/other/${post.title}`}>show</Link>
-            )}
-        </div>
+    return (
+        category ? (
+            <Link to={`${match.url}/${category.name}/${post.title}`}>
+                <div className={styles.post}>
+                    <div className={styles.post__card}>
+                        <h5 className={"mt-3 ml-3 text-capitalize text-white"}>{post.title}</h5>
+                        <p className={"p-3 text-white-100"}>{ trimText(post.body) }</p>
+                    </div>
+                </div>
+            </Link>
+        ) : (
+            <Link to={`${match.url}/other/${post.title}`}>
+                <div className={styles.post}>
+                    <div className={styles.post__card}>
+                        <h5 className={"mt-3 ml-3 text-capitalize text-white"}>{post.title}</h5>
+                        <p className={"p-3 text-white-100"}>{ trimText(post.body) }</p>
 
-    </div>
-  );
+                    </div>
+                </div>
+            </Link>
+        )
+    );
 }
 
 PostItem.propTypes = {
